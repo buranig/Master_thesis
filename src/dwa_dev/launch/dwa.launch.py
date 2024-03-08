d7a7fd4474682fea427254dff5370663d450e6f3
Jaume Albardaner i Torras <jaal5534@colorado.edu>
2024-03-08 16:05:52 +0100

    :sparkles: Launch file implemented

--- src/dwa_dev/launch/dwa.launch.py
+++ src/dwa_dev/launch/dwa.launch.py
@@ -3,33 +3,54 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
 
 
-# def generate_launch_description():
-#     track_yaml_path = os.path.join(
-#         os.path.expanduser('~'), 'driving_tracks', 'la_track.yaml'
-#     )
+def generate_launch_description():
+    generate_traj_condition = DeclareLaunchArgument(
+        'gen_traj',
+        description='whether trajectories are generated (True) for the current DWA configuration  or not (False)',
+        default_value='False'
+    )
 
-#     main_control_node = Node(
-#         package='main_control_racing_ros2',
-#         executable='amzmini_control_node2',
-#         name='main_control',
-#         parameters=[
-#             {'track_yaml': track_yaml_path},
-#             {'state_source': 'real'},
-#             {'control_target': 'real'},
-#             {'control_mode': 'pursuit'},
-#             {'activate': False},
-#             {'arm_mpc': False},
-#         ],
-#     )
 
-#     joy_node = Node(
-#         package='joy',
-#         executable='joy_node'
-#     )
+    config_yaml = os.path.join(
+        get_package_share_directory('dwa_dev'),
+        'config',
+        'dwa_config.yaml'
+    )
 
-#     return LaunchDescription([
-#         main_control_node,
-#         joy_node
-#     ])
+    traj_yaml = os.path.join(
+        get_package_share_directory('dwa_dev'),
+        'config',
+        'trajectories.yaml'
+    )
+
+    dwa_node = Node(
+        package='dwa_dev',
+        executable='dwa',
+        name='dwa_simulation',
+        parameters=[
+            {'car_yaml': config_yaml},
+            {'traj_yaml': traj_yaml}
+        ],
+    )
+    
+    traj_node = Node(
+        package='dwa_dev',
+        executable='generate_trajectories',
+        name='traj_generator',
+        parameters=[
+            {'car_yaml': config_yaml},
+        ],
+        condition=IfCondition(LaunchConfiguration('gen_traj'))
+    )
+
+
+    return LaunchDescription([
+        generate_traj_condition,
+        dwa_node,
+        traj_node
+    ])

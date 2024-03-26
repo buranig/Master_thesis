f41ce88e0f64be1783e23c77779f7522c8353b91
Jaume Albardaner i Torras <jaal5534@colorado.edu>
2024-03-26 18:03:49 +0100

    Finished control skeleton

--- bumper_cars/bumper_cars/collision_avoidance_ros2.py
+++ bumper_cars/bumper_cars/collision_avoidance_ros2.py
@@ -26,6 +26,10 @@ controller_map = {
 }
 
 
+from lar_msgs.msg import CarControlStamped
+from lar_msgs.srv import EnvState, CarCommand
+
+
 class CollisionAvoidance(Node):
     def __init__(self):
         super().__init__('ca')
@@ -33,20 +37,59 @@ class CollisionAvoidance(Node):
         self.declare_parameter('car_yaml', rclpy.Parameter.Type.STRING)
         self.declare_parameter('alg', rclpy.Parameter.Type.STRING)
 
+        # Init variables
         self.car_i = self.get_parameter('car_i').value
         self.car_yaml = self.get_parameter('car_yaml').value
         self.car_alg = self.get_parameter('alg').value
 
-        self.algorithm = controller_map[self.car_alg]
+        self.car_str = '' if self.car_i == 1 else str(self.car_i)
 
+        # Init controller
+        self.algorithm = controller_map[self.car_alg](self.car_yaml,self.car_i)
 
+        # Service to query state
+        self.state_cli = self.create_client(EnvState, 'env_state')
+        self.cmd_cli = self.create_client(CarCommand, 'car_cmd')
+        while not self.state_cli.wait_for_service(timeout_sec=1.0) or not self.cmd_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        
+        # Initialize service messages
+        self.state_req = EnvState.Request()
+        self.cmd_req = CarCommand.Request()
+        self.cmd_req.car = self.car_i - 1
 
+        self.publisher_ = self.create_publisher(CarControlStamped, '/sim/car'+self.car_str+'/set/control', 10)
+
+        
+
+    def state_request(self):
+        self.future = self.state_cli.call_async(self.state_req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+        
+    def cmd_request(self):
+        self.cmd_future = self.cmd_cli.call_async(self.cmd_req)
+        rclpy.spin_until_future_complete(self, self.cmd_future)
+        return self.cmd_future.result()
+        
+
+    def run(self):        
+        while rclpy.ok():
+            # set_comm = CarControlStamped()
+
+            des_action = self.cmd_request()
+            curr_state = self.state_request()
+            
+            
+            self.publisher_.publish(des_action.cmd)
+        
 
 def main(args=None):
     rclpy.init(args=args)
     node = CollisionAvoidance()
 
-    rclpy.spin(node)
-    node.destroy_node()
+    # Run control alg
+    node.run()
     
+    node.destroy_node()
     rclpy.shutdown()

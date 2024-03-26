425a3e1d84ccd9cab34e17fba6c672e9714b7210
Jaume Albardaner i Torras <jaal5534@colorado.edu>
2024-03-26 18:03:13 +0100

    included launching control from launch file + global vars

--- bumper_cars/bumper_cars/classes/Controller.py
+++ bumper_cars/bumper_cars/classes/Controller.py
@@ -17,32 +17,44 @@ class Controller:
     controller algorithms based on the specified controller type.
     """
 
-    def __init__(self, controller_path, id = 1):
+    # Initialize global parameters
+
+    dt = 0.0
+    L_d = 0.0
+    max_acc = 0.0
+    min_acc = 0.0
+
+    safety = 0.0
+    add_noise = 0.0
+    noise_scale = 0.0
+
+
+    def __init__(self, controller_path :str):
         
         with open(controller_path, 'r') as openfile:
             yaml_object = yaml.safe_load(openfile)
         
         # Global variables
-        self.dt = yaml_object["dt"]
-        self.L_d = yaml_object["L_d"]
-        self.max_acc = yaml_object["max_acc"]
-        self.min_acc = yaml_object["min_acc"]
+        self.dt = yaml_object["Controller"]["dt"]
+        self.L_d = yaml_object["Controller"]["L_d"]
+        self.max_acc = yaml_object["Controller"]["max_acc"]
+        self.min_acc = yaml_object["Controller"]["min_acc"]
 
-        self.safety = yaml_object["safety"]
-        self.add_noise = yaml_object["add_noise"]
-        self.noise_scale = yaml_object["noise_scale"]
-
-
+        self.safety = yaml_object["Controller"]["safety"]
+        self.add_noise = yaml_object["Controller"]["add_noise"]
+        self.noise_scale = yaml_object["Controller"]["noise_scale"]
 
 
-    # def compute_cmd(self, car_list : List[CarModel]) -> CarControlStamped:
-    #     try:
-    #         self.controller.compute_cmd(car_list)
-    #     except:    
-    #         raise Exception("Hereditary function doesn't implement 'compute_cmd'")
+
+
+    def compute_cmd(self, car_list : List[CarModel]) -> CarControlStamped:
+        try:
+            self.controller.compute_cmd(car_list)
+        except:    
+            raise Exception("Hereditary function doesn't implement 'compute_cmd'")
     
-    # def _add_noise(self, car_list : List[CarModel]) -> List[CarModel]:
-    #     try:
-    #         self.controller._add_noise(car_list)
-    #     except:
-    #         raise Exception("Hereditary function doesn't implement '_add_noise'")
+    def _add_noise(self, car_list : List[CarModel]) -> List[CarModel]:
+        try:
+            self.controller._add_noise(car_list)
+        except:
+            raise Exception("Hereditary function doesn't implement '_add_noise'")
--- bumper_cars/launch/collision-avoidance.launch.py
+++ bumper_cars/launch/collision-avoidance.launch.py
@@ -3,8 +3,9 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def add_car(context, ld):
@@ -17,7 +18,16 @@ def add_car(context, ld):
         'controller.yaml'
     )
 
+    track_yaml = os.path.join(
+        get_package_share_directory('lar_utils'),
+        'config',
+        'track',
+        'la_track.yaml'
+    )
+
+
     for i in range(int(param_value)):
+        #
         node = Node(
             package='bumper_cars',
             executable='collision_avoidance_ros2',
@@ -32,6 +42,28 @@ def add_car(context, ld):
         )
         ld.add_action(node)
 
+        # Control node
+        car_i = '' if i==0 else str(i+1)
+        node = Node(
+            package='main_control_racing_ros2',
+            executable='amzmini_control_node2',
+            name='main_control_node' + str(i + 1),
+            parameters=[
+                    {'track_yaml': track_yaml},
+                    # {'static_throttle': 0.5},
+                    {'control_mode': 'pursuit'},
+                    {'state_source': LaunchConfiguration('source_target')},
+                    {'control_target': LaunchConfiguration('source_target')},
+                    {'arm_mpc': False},
+                    {'carNumber': i + 1}
+            ],
+                remappings=[('/sim/car'+car_i+'/set/control', '/sim/car'+car_i+'/desired_control') ],
+            emulate_tty=True,
+            output='both'
+        )
+        ld.add_action(node)
+
+
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -40,6 +72,12 @@ def generate_launch_description():
         'carNumber',
         description='number of cars to be controlled, can be sim or real',
         default_value='1'
+    )
+
+    source_target_arg = DeclareLaunchArgument(
+        'source_target',
+        description='source of the information, can be sim or real',
+        default_value='sim'
     )
 
     node = Node(
@@ -54,6 +92,7 @@ def generate_launch_description():
     )
 
     ld.add_action(carNumber_arg)
+    ld.add_action(source_target_arg)
     ld.add_action(node)
     ld.add_action(OpaqueFunction(function=add_car, args=[ld]))
 

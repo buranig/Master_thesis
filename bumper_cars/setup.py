fece7a65b58c6e31dd45f801a5f800d18b198907
Jaume Albardaner i Torras <jaal5534@colorado.edu>
2024-05-08 16:13:54 +0200

    Rollback to python build

---
+++ bumper_cars/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+
+package_name = 'bumper_cars'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools', 'lar_msgs', 'lar_utils', 'rosidl_default_generators',  'dwa_dev'],
+    zip_safe=True,
+    maintainer='giacomo',
+    maintainer_email='buranig@student.ethz.ch',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "collision_avoidance_ros2 = bumper_cars.collision_avoidance_ros2:main",
+            "state_buffer = bumper_cars.state_buffer:main",
+        ],
+    },
+)
